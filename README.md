964ec4446824716aab490a08ba4b9001c7347889
Denis Parfenov <Parfenix@gmail.com>
2024-06-30 01:01:48 +0300

    Added logic for selecting games to the method of the main App class. I created a separate class for the game "Parity Check". Added the necessary infrastructure to run games.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+# Brain Games
+
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Parfenix/java-project-61/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Parfenix/java-project-61/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/9537e382a24c57c90029/maintainability)](https://codeclimate.com/github/Parfenix/java-project-61/maintainability)
+
+## Game 1: Checking the parity of a number
+### victory
+[![asciicast](https://asciinema.org/a/kqEzBz4lGtlrss9V3tDQKthuv.svg)](https://asciinema.org/a/kqEzBz4lGtlrss9V3tDQKthuv)
+### defeat
+[![asciicast](https://asciinema.org/a/qAGqiXURwe1neu6eqqpOuSSL2.svg)](https://asciinema.org/a/qAGqiXURwe1neu6eqqpOuSSL2)
---
+++ app/src/main/java/hexlet/code/EvenGame.java
@@ -0,0 +1,39 @@
+package hexlet.code;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class EvenGame {
+    public static void start() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        String username = scanner.nextLine();
+        System.out.println("Hello, " + username + "!");
+        System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
+
+        Random random = new Random();
+        int round = 0;
+        while (round < 3) {
+            int number = random.nextInt(100);
+            System.out.println("Question: " + number);
+            System.out.print("Your answer: ");
+            String answer = scanner.nextLine();
+            String correctAnswer = number % 2 == 0 ? "yes" : "no";
+
+            if (answer.equals(correctAnswer)) {
+                System.out.println("Correct!");
+                round++;
+            } else {
+                System.out.println("'" + answer + "' is wrong answer ;(. Correct answer was '" + correctAnswer + "'.");
+                break;
+            }
+        }
+
+        String win = "Congratulations, " + username + "!";
+        String lose = "Let's try again, " + username + "!";
+        String outro = round == 3 ? win : lose;
+
+        System.out.println(outro);
+    }
+}
